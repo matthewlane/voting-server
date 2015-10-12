ec79b0418e40f7904dfad1c4df2971c6c6df868e
Matt Lane <matthewlane@gmail.com>
2015-10-12 15:16:57 -0700

    Fix typo in key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export const store = makeStore();
 startServer(store);
 
 store.dispatch({
-    type: 'SET_ENTRIES',
-    entires: require('./entries.json')
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
 });
 store.dispatch({type: 'NEXT'});
